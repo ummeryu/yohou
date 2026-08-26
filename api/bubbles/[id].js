6e917699be779579d47915e891b85c0eb64cc77c
ummeryu <2752318348@qq.com>
2026-08-26 19:04:05 +0800

    11

--- api/bubbles/[id].js
+++ api/bubbles/[id].js
@@ -1,5 +1,6 @@
 const bubblesHandler = require('../../bubbles');
 
 module.exports = async function handler(req, res){
+  req.query = Object.assign({}, req.query, {id: req.query.id || req.params?.id});
   return bubblesHandler(req, res);
 };
